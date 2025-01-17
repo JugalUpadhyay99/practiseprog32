f49e8f7503c379687fa728ab0ed23b756fdc411f
Jugal Upadhyay <jugalupadhyay1212@gmail.com>
2025-01-17 12:18:59 +0530

    Add project files.

---
+++ practiseprog32/Controllers/UserController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace practiseprog32.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public IActionResult GetAllData()
+        {
+            try
+            {
+                CrudApiMasterContext crudApiMasterContext = new CrudApiMasterContext();
+                var users = crudApiMasterContext.UserMasters.ToList();
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpPut]
+        [Route("UpdateUser/{UserId}")]
+        public IActionResult UpdateUser(int id, UserMaster userUpdate)
+        {
+            try
+            {
+                CrudApiMasterContext crudApiMasterContext = new CrudApiMasterContext();
+                var user = crudApiMasterContext.UserMasters.FirstOrDefault(y => y.UserId == id);
+                user.FirstName = userUpdate.FirstName;
+                user.MiddleName = userUpdate.MiddleName;
+                user.LastName = userUpdate.LastName;
+                user.UserName = userUpdate.UserName;
+                user.Password = userUpdate.Password;
+                crudApiMasterContext.SaveChanges();
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser/{UserId}")]
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+                CrudApiMasterContext crudApiMasterContext = new CrudApiMasterContext();
+                var user = crudApiMasterContext.UserMasters.FirstOrDefault(y=>y.UserId == id);
+                crudApiMasterContext.UserMasters.Remove(user);
+                crudApiMasterContext.SaveChanges();
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("SaveUser")]
+        public IActionResult SaveUser(UserMaster userMaster)
+        {
+            try
+            {
+                CrudApiMasterContext crudApiMasterContext = new CrudApiMasterContext();
+                crudApiMasterContext.UserMasters.Add(userMaster);
+                crudApiMasterContext.SaveChanges();
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetAllUsersById/{id}")]
+        public IActionResult GetAllData(int id)
+        {
+            try
+            {
+                CrudApiMasterContext crudApiMasterContext = new CrudApiMasterContext();
+                var users = crudApiMasterContext.UserMasters.FirstOrDefault(y => y.UserId == id);
+
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ practiseprog32/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace practiseprog32.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
